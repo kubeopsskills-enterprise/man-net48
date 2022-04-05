acb3e2f7f15909bf8d9bfc3b64611c57f11e221f
wasuwat-kubeops <wasuwat.p@kubeops.guru>
2022-04-05 18:35:37 +0700

    Add: app setting to the HomeController and its view

--- SimpleNetFrame/Controllers/HomeController.cs
+++ SimpleNetFrame/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Configuration;
 
 namespace SimpleNetFrame.Controllers
 {
@@ -11,6 +12,8 @@ namespace SimpleNetFrame.Controllers
         public ActionResult Index()
         {
             ViewBag.Title = "Home Page";
+
+            ViewBag.Secret = ConfigurationManager.AppSettings["SomeSecret"];
 
             return View();
         }
